cbea996bf17a176b47e281efdf3d180f6c2b27dd
Petersil1998 <silbernagl.peter@rolmail.net>
2021-01-15 00:01:05 +0100

    Added german translation 2

--- src/main/java/net/petersil98/utilcraft/datagen/UtilcraftLanguages.java
+++ src/main/java/net/petersil98/utilcraft/datagen/UtilcraftLanguages.java
@@ -125,93 +125,93 @@ public class UtilcraftLanguages {
 
         @Override
         protected void addTranslations() {
-            add(UtilcraftBlocks.GOLD_BRICK, "Gold Ziegel");
-            add(UtilcraftBlocks.GOLD_STAIRS, "Gold Treppen");
-            add(UtilcraftBlocks.GOLD_SLAB, "Gold Slab");
-            add(UtilcraftBlocks.GOLD_WALL, "Gold Wall");
-            add(UtilcraftBlocks.COMPRESSED_COBBLESTONE, "Compressed Cobblestone");
-            add(UtilcraftBlocks.SILVER_ORE, "Silver Ore");
-            add(UtilcraftBlocks.ROSE_QUARTZ_ORE, "Rose Quartz Ore");
-            add(UtilcraftBlocks.ROSE_QUARTZ_BLOCK, "Rose Quartz Block");
-            add(UtilcraftSideSlabs.SIDE_STONE_SLAB, "Side Stone Slab");
-            add(UtilcraftSideSlabs.SIDE_COBBLESTONE_SLAB, "Side Cobblestone Slab");
-            add(UtilcraftSideSlabs.SIDE_OAK_SLAB, "Side Oak Slab");
-            add(UtilcraftSideSlabs.SIDE_SPRUCE_SLAB, "Side Spruce Slab");
-            add(UtilcraftSideSlabs.SIDE_BIRCH_SLAB, "Side Birch Slab");
-            add(UtilcraftSideSlabs.SIDE_JUNGLE_SLAB, "Side Jungle Slab");
-            add(UtilcraftSideSlabs.SIDE_ACACIA_SLAB, "Side Acacia Slab");
-            add(UtilcraftSideSlabs.SIDE_DARK_OAK_SLAB, "Side Dark Oak Slab");
-            add(UtilcraftSideSlabs.SIDE_SAKURA_SLAB, "Side Sakura Slab");
-            add(UtilcraftSideSlabs.SIDE_GOLD_SLAB, "Side Gold Slab");
-            add(UtilcraftBlocks.SAKURA_LEAVES, "Sakura Leaves");
-            add(UtilcraftBlocks.SAKURA_LOG, "Sakura Log");
-            add(UtilcraftBlocks.SAKURA_PLANKS, "Sakura Planks");
-            add(UtilcraftBlocks.SAKURA_SAPLING, "Sakura Sapling");
-            add(UtilcraftBlocks.SAKURA_SLAB, "Sakura Slab");
-            add(UtilcraftBlocks.SAKURA_STAIRS, "Sakura Stairs");
-            add(UtilcraftBlocks.SAKURA_FENCE, "Sakura Fence");
-            add(UtilcraftBlocks.SAKURA_FENCE_GATE, "Sakura Fence Gate");
-            add(UtilcraftBlocks.SAKURA_PRESSURE_PLATE, "Sakura Pressure Plate");
-            add(UtilcraftBlocks.SAKURA_TRAPDOOR, "Sakura Trapdoor");
-            add(UtilcraftBlocks.SAKURA_SIGN, "Sakura Sign");
-            add(UtilcraftBlocks.SAKURA_BUTTON, "Sakura Button");
-            add(UtilcraftBlocks.SAKURA_DOOR, "Sakura Door");
-            add(UtilcraftBlocks.DISENCHANTMENT_TABLE, "Disenchantment Table");
-            add(UtilcraftBlocks.SECURE_CHEST, "Secure Chest");
-            add(UtilcraftBlocks.REDSTONE_STAIRS, "Redstone Stairs");
-            add(UtilcraftBlocks.REDSTONE_SLAB, "Redstone Slab");
-            add(UtilcraftSideSlabs.SIDE_REDSTONE_SLAB, "Side Redstone Slab");
+            add(UtilcraftBlocks.GOLD_BRICK, "Gold Ziegelstein");
+            add(UtilcraftBlocks.GOLD_STAIRS, "Goldtreppe");
+            add(UtilcraftBlocks.GOLD_SLAB, "Goldstufe");
+            add(UtilcraftBlocks.GOLD_WALL, "Goldmauer");
+            add(UtilcraftBlocks.COMPRESSED_COBBLESTONE, "Gepresster Bruchstein");
+            add(UtilcraftBlocks.SILVER_ORE, "Silbererz");
+            add(UtilcraftBlocks.ROSE_QUARTZ_ORE, "Rosenquarzerz");
+            add(UtilcraftBlocks.ROSE_QUARTZ_BLOCK, "rosenquarzblock");
+            add(UtilcraftSideSlabs.SIDE_STONE_SLAB, "Seitliche Steinstufe");
+            add(UtilcraftSideSlabs.SIDE_COBBLESTONE_SLAB, "Seitliche Bruchsteinstufe");
+            add(UtilcraftSideSlabs.SIDE_OAK_SLAB, "Seitliche Eichenholzstufe");
+            add(UtilcraftSideSlabs.SIDE_SPRUCE_SLAB, "Seitliche Fichtenholzstufe");
+            add(UtilcraftSideSlabs.SIDE_BIRCH_SLAB, "Seitliche Brikenholzstufe");
+            add(UtilcraftSideSlabs.SIDE_JUNGLE_SLAB, "Seitliche Tropenholzstufe");
+            add(UtilcraftSideSlabs.SIDE_ACACIA_SLAB, "Seitliche Akazienholzstufe");
+            add(UtilcraftSideSlabs.SIDE_DARK_OAK_SLAB, "Seitliche Schwarzeichenholzstufe");
+            add(UtilcraftSideSlabs.SIDE_SAKURA_SLAB, "Seitliche Kirschblütenholzstufe");
+            add(UtilcraftSideSlabs.SIDE_GOLD_SLAB, "Seitliche Goldstufe");
+            add(UtilcraftBlocks.SAKURA_LEAVES, "Kirschblütenlaub");
+            add(UtilcraftBlocks.SAKURA_LOG, "Kirschblütenstamm");
+            add(UtilcraftBlocks.SAKURA_PLANKS, "Kirschblütenholzbretter");
+            add(UtilcraftBlocks.SAKURA_SAPLING, "Kirschblütensetzling");
+            add(UtilcraftBlocks.SAKURA_SLAB, "Kirschblütenholzstufe");
+            add(UtilcraftBlocks.SAKURA_STAIRS, "Kirschblütenholztreppe");
+            add(UtilcraftBlocks.SAKURA_FENCE, "Kirschblütenholzzaun");
+            add(UtilcraftBlocks.SAKURA_FENCE_GATE, "Kirschblütenholzzauntor");
+            add(UtilcraftBlocks.SAKURA_PRESSURE_PLATE, "Kirschblütenholzdruckplatte");
+            add(UtilcraftBlocks.SAKURA_TRAPDOOR, "Kirschblütenholzfalltür");
+            add(UtilcraftBlocks.SAKURA_SIGN, "Kirschblütenholzschild");
+            add(UtilcraftBlocks.SAKURA_BUTTON, "Kirschblütenholzknopf");
+            add(UtilcraftBlocks.SAKURA_DOOR, "Kirschblütenholztür");
+            add(UtilcraftBlocks.DISENCHANTMENT_TABLE, "Entzaubertisch");
+            add(UtilcraftBlocks.SECURE_CHEST, "Sichere Truhe");
+            add(UtilcraftBlocks.REDSTONE_STAIRS, "Redstonetreppe");
+            add(UtilcraftBlocks.REDSTONE_SLAB, "Redstonestufe");
+            add(UtilcraftSideSlabs.SIDE_REDSTONE_SLAB, "Seitliche Redstonestufe");
             add(UtilcraftBlocks.SUSHI_MAKER, "Sushi Maker");
 
-            add(UtilcraftItems.JUICER, "Juicer");
-            add(UtilcraftFoods.APPLE_JUICE, "Juicer");
-            add(UtilcraftFoods.SWEET_BERRY_JUICE, "Juicer");
-            add(UtilcraftItems.FLOUR, "Juicer");
-            add(UtilcraftFoods.BAGUETTE, "Juicer");
-            add(UtilcraftItems.ROSE_QUARTZ, "Juicer");
-            add(UtilcraftItems.ROSE_QUARTZ_HELMET, "Juicer");
-            add(UtilcraftItems.ROSE_QUARTZ_CHESTPLATE, "Juicer");
-            add(UtilcraftItems.ROSE_QUARTZ_LEGGINGS, "Juicer");
-            add(UtilcraftItems.ROSE_QUARTZ_BOOTS, "Juicer");
-            add(UtilcraftItems.ROSE_QUARTZ_PICKAXE, "Juicer");
-            add(UtilcraftItems.ROSE_QUARTZ_SHOVEL, "Juicer");
-            add(UtilcraftItems.ROSE_QUARTZ_HOE, "Juicer");
-            add(UtilcraftItems.ROSE_QUARTZ_SWORD, "Juicer");
-            add(UtilcraftItems.ROSE_QUARTZ_AXE, "Juicer");
-            add(UtilcraftItems.ROSE_QUARTZ_SUPER_HAMMER, "Juicer");
-            add(UtilcraftItems.ROSE_QUARTZ_SUPER_SHOVEL, "Juicer");
-            add(UtilcraftItems.TRAVELERS_BACKPACK, "Juicer");
-            add(UtilcraftItems.TNT_FINDER, "Juicer");
-            add(UtilcraftItems.SILVER_INGOT, "Juicer");
-            add(UtilcraftItems.BUTCHERS_KNIFE, "Butcher's Knife");
+            add(UtilcraftItems.JUICER, "Entsafter");
+            add(UtilcraftFoods.APPLE_JUICE, "Apfelsaft");
+            add(UtilcraftFoods.SWEET_BERRY_JUICE, "Süßbeerenssaft");
+            add(UtilcraftItems.FLOUR, "Mehl");
+            add(UtilcraftFoods.BAGUETTE, "Baguette");
+            add(UtilcraftItems.ROSE_QUARTZ, "Rosenquarz");
+            add(UtilcraftItems.ROSE_QUARTZ_HELMET, "Rosenquarzhelm");
+            add(UtilcraftItems.ROSE_QUARTZ_CHESTPLATE, "Rosenquarzharnisch");
+            add(UtilcraftItems.ROSE_QUARTZ_LEGGINGS, "Rosenquarzbeinschutz");
+            add(UtilcraftItems.ROSE_QUARTZ_BOOTS, "Rosenquarzstiefel");
+            add(UtilcraftItems.ROSE_QUARTZ_PICKAXE, "Rosenquarzspitzhacke");
+            add(UtilcraftItems.ROSE_QUARTZ_SHOVEL, "Rosenquarzschaufel");
+            add(UtilcraftItems.ROSE_QUARTZ_HOE, "Rosenquarzhacke");
+            add(UtilcraftItems.ROSE_QUARTZ_SWORD, "Rosenquarzschwert");
+            add(UtilcraftItems.ROSE_QUARTZ_AXE, "Rosenquarzaxt");
+            add(UtilcraftItems.ROSE_QUARTZ_SUPER_HAMMER, "Rosenquarzsuperhammer");
+            add(UtilcraftItems.ROSE_QUARTZ_SUPER_SHOVEL, "Rosenquarzsuperschaufel");
+            add(UtilcraftItems.TRAVELERS_BACKPACK, "Rucksack des Reisenden");
+            add(UtilcraftItems.TNT_FINDER, "TNT Sucher");
+            add(UtilcraftItems.SILVER_INGOT, "Silberbarren");
+            add(UtilcraftItems.BUTCHERS_KNIFE, "Metzgermesser");
 
-            add(UtilcraftEnchantments.BEHEADING, "Beheading");
+            add(UtilcraftEnchantments.BEHEADING, "Enthauptung");
 
             add("itemGroup.utilcraft", "Utilcraft");
 
-            add("advancements.utilcraft.root.title", "Green Thumb");
-            add("advancements.utilcraft.root.description", "Place a Sakura Sapling");
+            add("advancements.utilcraft.root.title", "Grüner Daumen");
+            add("advancements.utilcraft.root.description", "Setze einen Kirschblütensetzling");
 
-            add("advancements.utilcraft.mine_rose_quartz.title", "Mine Rose Quartz");
-            add("advancements.utilcraft.mine_rose_quartz.description", "Find and mine some Rose Quartz");
+            add("advancements.utilcraft.mine_rose_quartz.title", "Baue Rosenquarz ab");
+            add("advancements.utilcraft.mine_rose_quartz.description", "Finde und baue Rosenquarz ab");
 
-            add("screen.utilcraft.disenchantment_table", "Disenchantment Table");
-            add("screen.utilcraft.secure_chest", "Secure Chest");
+            add("screen.utilcraft.disenchantment_table", "Entzaubertisch");
+            add("screen.utilcraft.secure_chest", "Sichere Truhe");
 
-            add("protection.utilcraft.block_protected", "This block is protected, you cannot break or use it!");
-            add("player_trusted.utilcraft.player_added", "Player %s can now access your Secure Chests");
-            add("player_trusted.utilcraft.player_removed", "Player %s can no longer access your Secure Chests");
-            add("player_trusted.utilcraft.list", "You trust the following Players: %s");
+            add("protection.utilcraft.block_protected", "Dieser Block ist gesichert, du kannst ihn weder abbauen noch benutzen!");
+            add("player_trusted.utilcraft.player_added", "Der Spieler %s kann absofort auf deine Sicheren Truhen zugreifen!");
+            add("player_trusted.utilcraft.player_removed", "Der Spieler %s kann nicht mehr auf deine Sicheren Truhen zugreifen!");
+            add("player_trusted.utilcraft.list", "Du vertraust folgenden Spielern: %s");
 
-            add("vein_miner.utilcraft.active", "Vein Miner active");
-            add("vein_miner.utilcraft.inactive", "Vein Miner inactive");
+            add("vein_miner.utilcraft.active", "Vein Miner aktiv");
+            add("vein_miner.utilcraft.inactive", "Vein Miner inaktiv");
 
-            add("home.utilcraft.set_home", "Successfully set home to current location!");
-            add("home.utilcraft.not_set", "Home wasn't set yet!");
-            add("home.utilcraft.teleported", "Welcome Home!");
+            add("home.utilcraft.set_home", "Zuhause wurde erfolgreich auf die aktuelle Position gesetzt!");
+            add("home.utilcraft.not_set", "Zuhause wurde noch nicht gesetzt!");
+            add("home.utilcraft.teleported", "Willkommen zuhause!");
 
             add("key_bindings.utilcraft.category", "Utilcraft");
-            add("key_bindings.utilcraft.vein_miner", "Toggle Vein Miner");
+            add("key_bindings.utilcraft.vein_miner", "Vein Miner ein/ausschalten");
         }
     }
 }
