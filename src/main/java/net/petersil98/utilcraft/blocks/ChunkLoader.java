939b6832b57e48ed5214eecb6df98791e4180fde
Petersil1998 <silbernagl.peter@rolmail.net>
2021-02-02 22:00:54 +0100

    .

---
+++ src/main/java/net/petersil98/utilcraft/blocks/ChunkLoader.java
@@ -0,0 +1,47 @@
+package net.petersil98.utilcraft.blocks;
+
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.material.Material;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.server.ServerWorld;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+public class ChunkLoader extends Block {
+
+    public static final int RADIUS = 1;
+
+    public ChunkLoader() {
+        super(AbstractBlock.Properties
+                .create(Material.ROCK)
+        );
+    }
+
+    @Override
+    public void onBlockPlacedBy(@Nonnull World world, @Nonnull BlockPos pos, @Nonnull BlockState state, @Nullable LivingEntity placer, @Nonnull ItemStack stack) {
+        if(world instanceof ServerWorld) {
+            for(int i = 0; i < RADIUS; i++) {
+                Chunk chunk = world.getChunkAt(pos);
+                ((ServerWorld) world).forceChunk(chunk.getPos().x, chunk.getPos().z, true);
+            }
+        }
+    }
+
+    @Override
+    public void onReplaced(@Nonnull BlockState state, @Nonnull World worldIn, @Nonnull BlockPos pos, @Nonnull BlockState newState, boolean isMoving) {
+        if(worldIn instanceof ServerWorld) {
+            for(int i = 0; i < RADIUS; i++) {
+                Chunk chunk = worldIn.getChunkAt(pos);
+                ((ServerWorld) worldIn).forceChunk(chunk.getPos().x, chunk.getPos().z, false);
+            }
+        }
+    }
+}
